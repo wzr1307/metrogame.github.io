3deaa0c720d6b6c9447991987591626a756f6551
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-12-16 14:31:05 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openChat() {
+    document.getElementById("chat-modal").style.display = "block"; // Показываем модальное окно
+}
+
+function closeChat() {
+    document.getElementById("chat-modal").style.display = "none"; // Скрываем модальное окно
+}
