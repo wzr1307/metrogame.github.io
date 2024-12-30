68281576bb46f7be6fba589edbb5130d8497a2ca
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-12-30 22:32:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-function openChat() {
-    document.getElementById("chat-modal").style.display = "block"; // Показываем модальное окно
-}
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
 
-function closeChat() {
-    document.getElementById("chat-modal").style.display = "none"; // Скрываем модальное окно
-}
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
